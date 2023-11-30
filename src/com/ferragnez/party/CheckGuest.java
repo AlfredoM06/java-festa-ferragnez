2012d1f146dd63b3645867fe219679c8747cfaa5
AlfredoM06 <Alfredomanfredi06@gmail.com>
2023-11-30 16:43:54 +0100

    Exercise fixed

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,2 +1,31 @@
-package com.ferragnez.party;public class CheckGuest {
+package com.ferragnez.party;
+
+import java.util.Scanner;
+
+public class CheckGuest {
+    public static void main(String[] args) {
+        Scanner keyInput = new Scanner(System.in);
+
+        String[] guestList= {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+
+        System.out.println("Inserisci qui il tuo nome");
+        String guestName = keyInput.nextLine();
+
+        boolean found = false;
+        for (int i = 0; i < guestList.length && !found; i++) {
+            if (guestName.equals(guestList[i])) {
+                found = true;
+            }
+
+        }
+        if (found){
+            System.out.println("Prego si accomodi e si goda la festa");
+        } else {
+            System.out.println("Tornatene da dove sei venuto pezzente");
+        }
+
+
+        keyInput.close();
+
+    }
 }
